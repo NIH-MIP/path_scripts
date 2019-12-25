dafc3c7e32cdbdddf497fca992b2076035b94001
NIH-MIP <45637019+NIH-MIP@users.noreply.github.com>
2019-12-25 12:44:58 -0500

    Update heatmap_generator.py

--- heatmap_generator.py
+++ heatmap_generator.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from PIL import Image
+from matplotlib import cm
 
 
 class GenHeatmap:
@@ -8,29 +9,56 @@ class GenHeatmap:
     def __init__(self):
         self.basePATH='/Users/sanforth/Desktop/save_tiles'
 
-    def heatmap(self):
+    def multires_heatmap(self):
+        '''create multiresolution heatmap
+        :param: path(str) path to the
         '''
-        trying to make a heatmap at one resoultion first, then combining them should be simple.
+
+        path=self.basePATH #right now, only works for one patient.  Will need to add loop for multiple patients
+
+        #create dictionary with heatmap for every resolution
+        cor_dict = {'5x': 16, '10x': 8, '20x': 4}
+        array_dict={}
+        for res in os.listdir(path):
+            if res.startswith('.'): #for all the stoopid hidden filenames
+                pass
+            else:
+                correction=cor_dict[res]
+                array_dict[res]=self.heatmap(res,correction)
+
+        #combine the multiple resolutions
+        array=np.zeros(array_dict['5x'].shape)
+        for res in array_dict.keys():
+            print(res)
+            print(array_dict[res].shape)
+            self.display(array_dict[res])
+            array+=array_dict[res]
+
+        self.display(array)
+
+        return(array)
+
+    def heatmap(self,res='5x',correction=4):
+        '''
+        Create a nxm array that assigns predictions to
         :return:
         '''
 
-        res='5x'
+        #find all files, patch size, and unit size
         files=os.listdir(os.path.join(self.basePATH,res))
         size_patch=int(files[0].split('_')[3].split('-')[0])
-        us=int(size_patch/16) #us=unit size
+        us=int(size_patch/correction) #us=unit size
 
         #initialize empty numpy array
         file=self.find_largest_file(file_list=files)
-        xy_coord=file.split('_')[2];box_size=file.split('_')[3]
+        xy_coord=file.split('_')[2]; box_size=file.split('_')[3]
         x_coord=int(xy_coord.split('-')[0])+int(box_size.split('-')[0])
         y_coord = int(xy_coord.split('-')[1]) + int(box_size.split('-')[1])
-        x_coord_unit=int(x_coord/us); y_coord_unit=int(y_coord/us)
-        array = np.array([range(0,1)] * x_coord_unit*y_coord_unit).reshape((x_coord_unit, y_coord_unit))
-        print(array.shape)
+        array=np.zeros((int(x_coord/us),int(y_coord/us)))
 
+        #iterate over files and
         for file in files:
-            pred=float(file.split('_')[4].split('-')[1].split('.png')[0])
-            pred=pred*255 if pred<1 else pred
+            pred=float(file.split('_')[4].split('-')[1].split('.png')[0]) #will need to change this line for prediction --> right now just using white space predictions
             xy_coord=file.split('_')[2]; xy_size=file.split('_')[3]
 
             #select out the corredinates of the submatrix you are interested in
@@ -39,21 +67,15 @@ class GenHeatmap:
             botR_coord_x= int(topL_coord_x+ int(xy_size.split('-')[0])/us)
             botR_coord_y=int(topL_coord_y+int(xy_size.split('-')[1])/us)
 
-            #assign the
-            array_sm=array[topL_coord_x:botR_coord_x,topL_coord_y:botR_coord_y]
-
+            #assign the prediction to the array
             array[topL_coord_x:botR_coord_x,topL_coord_y:botR_coord_y]=pred
 
-        print(array.shape)
-        print(type(array))
-        img = Image.fromarray(array,mode='L')
-        img.show()
         return array
 
 
     def find_largest_file(self,file_list):
         '''
-        helper function to find the largest x and y position
+        helper function to find the largest x and y position among filenames in a give directory
         :return:
         '''
         largest_sum=0
@@ -66,11 +88,14 @@ class GenHeatmap:
                 largest_file=file
         return largest_file
 
-
-
+    def display(self,array):
+        '''quick helper function to display image array'''
+        im = Image.fromarray(np.uint8(cm.gist_earth(array) * 255))
+        im=im.rotate(270,expand=True)
+        im=im.transpose(Image.FLIP_LEFT_RIGHT)
+        im.show()
 
 
 if __name__=='__main__':
     c=GenHeatmap()
-    c.heatmap()
-
+    c.multires_heatmap()
